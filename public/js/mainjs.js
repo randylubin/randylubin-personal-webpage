1781a3523e58eb08817bd59afd97230f4306fdcc
Randy Lubin <randylubin@gmail.com>
2012-05-06 14:53:19 -0700

    Added Animation

--- public/js/mainjs.js
+++ public/js/mainjs.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+	$('#maincont').delay(200).animate({marginTop: '+=700'},800)
+					.animate({marginTop: '-=35'},300)
+					.animate({marginTop: '+=25'},400)
+					.animate({marginTop: '+=10'},200);
+
 	console.log("start!");
 	if ($("#creditfooter")) {
 		$("#creditfooter").remove();
