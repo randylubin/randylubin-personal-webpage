740dfeed77ec9aaf501a8e661fb1d7133181d87a
Randy Lubin <randylubin@gmail.com>
2012-05-08 10:03:41 -0700

    updated animation

--- public/js/mainjs.js
+++ public/js/mainjs.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('#maincont').delay(200).animate({marginTop: '+=700'},800)
 					.animate({marginTop: '-=35'},300)
-					.animate({marginTop: '+=25'},400)
-					.animate({marginTop: '+=10'},200);
+					.animate({marginTop: '+=35'},400);
 
-	console.log("start!");
+
+
 	if ($("#creditfooter")) {
 		$("#creditfooter").remove();
 		console.log("done!");
